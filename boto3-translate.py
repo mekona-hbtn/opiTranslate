90c1180385fb929ab52a6a7a24bedffa9c873dc4
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2022-06-18 16:53:27 -0500

    frat: Added CLI to boto

--- boto3-translate.py
+++ boto3-translate.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 
 import boto3
+import sys
 
 translate = boto3.client(service_name='translate', region_name='us-east-1', use_ssl=True)
+argv = sys.argv
 
-result = translate.translate_text(Text="Hello, World", 
-            SourceLanguageCode="en", TargetLanguageCode="es")
+result = translate.translate_text(Text=argv[1], 
+            SourceLanguageCode=argv[2], TargetLanguageCode=argv[3])
 print('TranslatedText: ' + result.get('TranslatedText'))
 print('SourceLanguageCode: ' + result.get('SourceLanguageCode'))
 print('TargetLanguageCode: ' + result.get('TargetLanguageCode'))
--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 
 if __name__ == '__main__':
     translateAPI = __import__('translate').translateAPI
-    text = {"text": "hola mundo"}
+    text = {"from": "es",
+            "to": "de",
+            "text": "hola mundo"}
 
     print(translateAPI(text))
