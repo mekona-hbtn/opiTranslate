175b80cf43aa22cc6d25a5db0e65cc7d4d95ae7c
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-06-27 14:46:26 -0500

    feat: handles the translation from html button

--- views/profile.py
+++ views/profile.py
@@ -1,30 +1,51 @@
 #!/usr/bin/env python3
-import requests
-from flask import Blueprint, render_template
+"""
+Profile view
+==========================
+This module contains the logic behind the profiles of the candidates in the
+offer. It selects the database to work with and connects to the AWS Translate
+API to translate the interview.
+"""
 
+# Core app & Flask imports
+from bson import ObjectId
+import requests
+from flask import Blueprint, render_template, abort, request, make_response
+
+# Creates the blueprint of the profile view
 profile = Blueprint('profile', __name__)
 
 
-@profile.route('/profile', strict_slashes=False)
-def candidate():
-    from app import techie_info
+@profile.route('/profile/<id>', methods=['GET', 'POST'], strict_slashes=False)
+def candidate(id):
+    from app import test
 
-    # Getting the DB info
-    techie = techie_info.find_one({'alias': 'Opgrounder 3438'})
+    # Gets the techi info from the DB by id
+    by_id = {'_id': ObjectId(id)}
+    techie = test.techie_info.find_one(by_id)
+    if not techie:
+        abort(404)
     interview = techie['interview']
 
-    # Request translation to AWS API
-    url = 'https://448zpz6x43.execute-api.us-east-1.amazonaws.com/opi/opi'
-    translation = {}
-    for key, value in interview.items():
-        translate = {"text": value}
-        translation[key] = requests.post(url, json=translate).text
-
-    # Preparing variables to be available in the context of the template
+    # Prepares variables to be available in the context of the template
     context = {
         'techie': techie,
         'interview': interview,
-        'translation': translation
     }
 
+    # Handles the Translate button
+    if request.method == 'POST':
+
+        # Requests the interview translation to AWS Translate API
+        url = 'https://448zpz6x43.execute-api.us-east-1.amazonaws.com/opi/opi'
+        translation = {}
+        for key, value in interview.items():
+            translate = {"text": value}
+            value_translated = requests.post(url, json=translate).text
+            translation[key] = value_translated.replace('"', '')
+
+        # Saves the translation in the context interview variable
+        context['interview'] = translation
+
+    # Sends the techi_info to the template
     return render_template('profile.html', **context)
