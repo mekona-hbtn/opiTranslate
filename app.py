a6cf42e8bcdf144ba456a4c4e06869b5ad8da1ce
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-06-27 11:46:31 -0500

    feat: set uri for mongodb & docs

--- app.py
+++ app.py
@@ -1,19 +1,37 @@
 #!/usr/bin/env python3
-import requests
+"""
+opiTranslation Application
+==========================
+This is the file where the execution of the application starts. The back-end
+follows the Flask framework, imports the necessary view blueprints and connects
+to a NoSQL database in MongoDB.
+
+Be sure to run this application in a virtual environment that has installed all
+the libraries of the file `requirements.txt`
+"""
+
+# Core Flask & MongoDB imports
 from flask import Flask
 from pymongo import MongoClient
+
+# Imports from the Blueprints of the app
 from views.profile import profile
 
-app = Flask(__name__)
-app.register_blueprint(profile)
+# Conects to the MongoDB server
+user, code, cluster = 'root', 'toor', 'opitranslate.dkaoilj.mongodb.net'
+uri = f'mongodb+srv://{user}:{code}@{cluster}/?retryWrites=true&w=majority'
 
-mongo = MongoClient(
-    'mongodb+srv://opitranslate.dkaoilj.mongodb.net',
-    username='root', password='toor')
+mongo = MongoClient(uri, serverSelectionTimeoutMS=5000)
 
-db = mongo['opground_test_db']
-techie_info = db['techie_info']
+# Gets the production and test databases
+test = mongo['opground_test_db']
+
 
 
 if __name__ == '__main__':
+    # Creates the application & register the blueprints
+    app = Flask(__name__)
+    app.register_blueprint(profile)
+
+    # Start the application
     app.run(port=5000, debug=True)
