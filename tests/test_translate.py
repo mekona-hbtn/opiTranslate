c9586dfac2bc5cb4f5a0664c3d2e1e727dd8713f
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-07-04 10:41:58 -0500

    fix: pycodestyle

--- tests/test_translate.py
+++ tests/test_translate.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
-import requests
 import os
+import requests
 from dotenv import load_dotenv
 
-load_dotenv('.env') 
+load_dotenv('.env')
+
 
 def test_tr1():
     text = {"text": "hola mundo"}
@@ -12,11 +13,13 @@ def test_tr1():
     value_translated = requests.post(URL, json=text).text
     assert value_translated == '"hello world"'
 
+
 def test_tr2():
     text = {"text": "bonjour le monde"}
     URL = os.environ.get('AWS_TRANSLATE')
     value_translated = requests.post(URL, json=text).text
     assert value_translated == '"hello world"'
+
 
 def test_tr3():
     text = {"text": "hallo welt"}
